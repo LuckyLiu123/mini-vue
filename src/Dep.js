3037381094a8305cedf6bd87d2d9990fd9146b04
liukai <15818524733@139.com>
2021-03-08 07:01:51 +0800

    xxx

--- src/Dep.js
+++ src/Dep.js
@@ -19,6 +19,27 @@ class Dep{
             }
         }
     }
+
+    /** 将当前 Dep 与当前的 Watcher(暂时为渲染 watcher)关联 */
+    depend(){
+        if(Dep.target){
+            this.addSub(Dep.target);  //将当前的 watcher 关联到当前的 dep 上
+            Dep.target.addDep(this);  //将当前的 dep 与当前的 渲染watcher 关联起来
+
+        }
+    }
+
+    /** 触发与之关联的 watcher 的 update 方法，起到更新的作用 */
+    notify(){
+        /**
+         * 在真实的 Vue 中是依次触发 this.subs 中的 watcher 的 update 方法
+         * 此时，deps 中已经关联到 我们需要使用的 那个 watcher 了
+        */
+        let deps = this.subs.slice();
+        deps.forEach(watcher => {
+            watcher.update();
+        })
+    }
 }
 
 //全局的容器存储渲染 watcher
--- src/initData.js
+++ src/initData.js
@@ -1,4 +1,22 @@
+//数组的响应式化
+let ARRAY_METHOD = ['push', 'pop', 'shift', 'unshift', 'reverse', 'sort', 'splice'];
 
+let array_methods = Object.create(Array.prototype);
+
+//函数的扩展功能
+ARRAY_METHOD.forEach(method => {
+    array_methods[method] = function(){
+        console.log('调用拦截的 ' + method + ' 方法');
+
+        //将数据响应式化
+        for(let i = 0; i < arguments.length; i++){
+            observe(arguments[i]);
+        }
+
+        let res = Array.prototype[method].apply(this, arguments);
+        return res;
+    }
+})
 
 function defineReactive(target, key, value, enumerable){
     //函数内部就是一个局部作用域，这个 value 就只在函数内使用的变量（闭包）
@@ -7,16 +25,29 @@ function defineReactive(target, key, value, enumerable){
         observe(value);
     }
 
+    let dep = new Dep();
+    dep.__dropName__ = key;
+
     Object.defineProperty(target, key, {
         configurable: true,
         enumerable: !!enumerable,
         get(){
             console.log(`读取 ${key} 属性`);
+            //依赖收集
+            dep.depend();
             return value;
         },
         set(newVal){
             console.log(`设置 ${key} 属性`);
+
+            //目的: 将重新赋值的数据变成响应式得，因此如果传入的是对象类型，那么就需要使用 observe 将其转换为响应式的
+            if(typeof newVal === 'object' && newVal != null){
+                observe(newVal);
+            }
             value = newVal;
+
+            //派发更新，找到全局的 watcher，调用 update
+            dep.notify();
         }
     })
 }
@@ -26,6 +57,7 @@ function observe(obj, vm){
     //之前没有对 obj 本身进行操作，这一次就直接对 obj 进行判断
     if(Array.isArray(obj)){
         // 对其每一个元素进行处理
+        obj.__proto__ = array_methods;
         for(let i = 0; i < obj.length; i++){
             observe(obj[i]);   //递归处理每一个数组元素
         }
@@ -40,7 +72,33 @@ function observe(obj, vm){
 }
 
 JGVue.prototype.initData = function(){
+    //遍历 this._data 的成员，将属性转换为响应式，将直接属性，代理到实例上
+    let keys = Object.keys(this._data);
 
     //响应式化
     observe(this._data, this);
+
+    //代理
+    for(let i = 0; i < keys.length; i++){
+        /**
+         * 将 this._data[keys[i]] 映射到 this[keys[i]] 上
+         * 就是要让 this 提供 keys[i] 这个属性
+         * 在访问这个属性的时候 相当于在访问 this._data 上的这个属性
+        */
+       proxy(this, "_data", keys[i]);
+    }
+}
+
+/** 将 某一个对象的属性 访问 映射到 对象的某一个属性成员上 */
+function proxy(target, prop, key){
+    Object.defineProperty(target, key, {
+        enumerable: true,
+        configurable: true,
+        get(){
+            return target[prop][key];
+        },
+        set(newVal){
+            target[prop][key] = newVal;
+        }
+    })
 }
